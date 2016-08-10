4b4da6bae7bbb88c820c60817248597ac49d3c10
Kristian Larsson <kristian@spritelink.net>
2016-08-10 14:32:33 +0200

    vmx: Use lite image when available
    Older versions of vMX ship with two vFPC images - one is for performance and
    the other is the lite image meant for low throughput labing. The performance
    image requires some specific CPU instructions and I couldn't actually get it to
    work on my dev laptop, despite it being rather new, so switching to the lite
    image.
    
    vMX 14.1 is the only versions I've seen that actually has these two images.
    
    Fixes #30.

--- vmx/docker/install_vmx.sh
+++ vmx/docker/install_vmx.sh
@@ -7,7 +7,7 @@ mv -v /tmp/vmx*/images/vmxhdd.img /vmx/
 mv -v /tmp/vmx*/images/junos-vmx*qcow2 /vmx/		# 16.1 and newer
 mv -v /tmp/vmx*/images/jinstall64-vmx*img /vmx/
 # vFPC / vPFE
-mv -v /tmp/vmx*/images/vPFE-2*.img /vmx/vfpc.img	# 14.1
+mv -v /tmp/vmx*/images/vPFE-lite-*.img /vmx/vfpc.img	# 14.1
 mv -v /tmp/vmx*/images/vFPC*.img /vmx/vfpc.img		# 15.1 and newer
 mv -v /tmp/vmx*/images/metadata-usb-*.img /vmx/
 mv -v /tmp/vmx*/images/metadata_usb.img /vmx/metadata-usb-re.img	# old style

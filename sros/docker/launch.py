7d5435edb6edef7aeb66e50bd6729a3ab33aad1d
Kristian Larsson <kristian@spritelink.net>
2016-08-29 17:10:15 +0200

    sros: Refactor launch to use common class
    Just like what I've done with XR, the startup is now happening mostly in
    a common class and we only override parts of it for SROS specific
    functionality.
    
    Part of #28.

--- sros/docker/launch.py
+++ sros/docker/launch.py
@@ -33,27 +33,6 @@ def trace(self, message, *args, **kws):
         self._log(TRACE_LEVEL_NUM, message, args, **kws)
 logging.Logger.trace = trace
 
-def run_command(cmd, cwd=None, background=False):
-    res = None
-    try:
-        if background:
-            p = subprocess.Popen(cmd, cwd=cwd)
-        else:
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd)
-            res = p.communicate()
-    except:
-        pass
-    return res
-
-def gen_mac(last_octet=None):
-    """ Generate a random MAC address that is in the qemu OUI space and that
-        has the given last octet.
-    """
-    return "52:54:00:%02x:%02x:%02x" % (
-            random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff),
-            last_octet
-        )
 
 def mangle_uuid(uuid):
     """ Mangle the UUID to fix endianness mismatch on first part
@@ -81,91 +60,12 @@ def uuid_rev_part(part):
     return res
 
 
-class SROS(vrnetlab.VR):
-    def __init__(self):
-        self.logger = logging.getLogger()
-        self.spins = 0
-        self.vm_started = False
-
-        self.username = None
-        self.password = None
-
-        self.ram = 4096
-        self.num_nics = 20
-
-        self.uuid = "00000000-0000-0000-0000-000000000000"
-        self.license_start = None
-
-        self.p_vm = None
-
-        self.start_time = None
 
 
+class SROS_vm(vrnetlab.VM):
+    def __init__(self, username, password):
+        super(SROS_vm, self).__init__(username, password)
 
-    def read_license(self):
-        """ Read the license file, if it exists, and extract the UUID and start
-            time of the license
-        """
-        if not os.path.isfile("/tftpboot/license.txt"):
-            self.logger.info("No license file found")
-            return
-
-        lic_file = open("/tftpboot/license.txt", "r")
-        license = lic_file.read()
-        lic_file.close()
-        try:
-            uuid_input = license.split(" ")[0]
-            self.uuid = mangle_uuid(uuid_input)
-            m = re.search("([0-9]{4}-[0-9]{2}-)([0-9]{2})", license)
-            if m:
-                self.license_start = "%s%02d" % (m.group(1), int(m.group(2))+1)
-        except:
-            raise ValueError("Unable to parse license file")
-        self.logger.info("License file found for UUID %s with start date %s" % (self.uuid, self.license_start))
-
-
-
-    def start(self):
-        """ Start the virtual router
-
-            This can take a long time as we are waiting for the router to start
-            and the do initial bootstraping of it over serial port.
-        """
-        run_command(["socat", "TCP-LISTEN:22,fork", "TCP:127.0.0.1:2022"], background=True)
-        run_command(["socat", "TCP-LISTEN:830,fork", "TCP:127.0.0.1:2830"], background=True)
-        while True:
-            self.check_qemu()
-            if not self.vm_started:
-                self.bootstrap_spin()
-
-
-
-    def check_qemu(self):
-        """ Check health of qemu. This is mostly just seeing if there's error
-            output on STDOUT from qemu which means we restart it.
-        """
-        if self.p_vm is None:
-            self.start_vm()
-
-        # check for output
-        try:
-            outs, errs = self.p_vm.communicate(timeout=1)
-        except subprocess.TimeoutExpired:
-            return
-
-        if errs != "":
-            self.update_health(2, "KVM error. restarting VM")
-            self.stop_vm()
-            self.start_vm()
-
-
-
-    def start_vm(self):
-        """ Start the VM
-        """
-        self.vm_started = False
-        self.update_health(2, "starting VM")
-        self.start_time = datetime.datetime.now()
         # move files into place
         for e in os.listdir("/"):
             if re.search("\.qcow2$", e):
@@ -173,77 +73,18 @@ class SROS(vrnetlab.VR):
             if re.search("\.license$", e):
                 os.mkdir("/tftpboot")
                 os.rename("/" + e, "/tftpboot/license.txt")
+
+        self.disk_image = "/sros.qcow2"
+        self.num_fake_nics = 1
+        self.uuid = "00000000-0000-0000-0000-000000000000"
+
         self.read_license()
-
-        self.logger.info("Starting VM")
-        bof = "type=1,product=TIMOS:address=10.0.0.15/24@active license-file=tftp://10.0.0.2/license.txt slot=A chassis=SR-c12 card=cfm-xp-b mda/1=m20-1gb-xp-sfp mda/3=m20-1gb-xp-sfp mda/5=m20-1gb-xp-sfp"
-
-        cmd = ["qemu-system-x86_64", "-display", "none", "-m", str(self.ram),
-               "-serial", "telnet:0.0.0.0:5000,server,nowait", "-smbios", bof,
-               "-hda", "/sros.qcow2", "-uuid", self.uuid
-               ]
-        # enable hardware assist if KVM is available
-        if os.path.exists("/dev/kvm"):
-            cmd.insert(1, '-enable-kvm')
-
-        # do we have a license start date?
-        if self.license_start:
-            cmd.extend(["-rtc", "base=" + self.license_start])
-
-        # mgmt interface is special - we use qemu user mode network
-        cmd.append("-device")
-        cmd.append("e1000,netdev=mgmt,mac=%(mac)s"
-                   % { 'mac': gen_mac(0) })
-        cmd.append("-netdev")
-        cmd.append("user,id=mgmt,net=10.0.0.0/24,tftp=/tftpboot,hostfwd=tcp::2022-10.0.0.15:22,hostfwd=tcp::2830-10.0.0.15:830")
-        # dummy interface
-        cmd.extend(["-device", "e1000,netdev=dummy,mac=%s" % gen_mac(0)])
-        cmd.extend(["-netdev", "tap,ifname=vfpc-dummy,id=dummy,script=no,downscript=no"])
-
-        for i in range(1, self.num_nics):
-            cmd.append("-device")
-            cmd.append("e1000,netdev=p%(i)02d,mac=%(mac)s"
-                       % { 'i': i, 'mac': gen_mac(i) })
-            cmd.append("-netdev")
-            cmd.append("socket,id=p%(i)02d,listen=:100%(i)02d"
-                       % { 'i': i })
-
-        self.p_vm = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE,
-                                     universal_newlines=True)
-        try:
-            self.p_vm.communicate(timeout=5)
-        except subprocess.TimeoutExpired:
-            pass
-        time.sleep(5)
-
-        self.tn = telnetlib.Telnet("127.0.0.1", 5000)
-
-
-    def stop_vm(self):
-        self.vm_started = False
-        try:
-            self.p_vm.terminate()
-        except ProcessLookupError:
-            return
-
-        try:
-            self.p_vm.communicate(timeout=10)
-        except:
-            self.p_vm.kill()
-            self.p_vm.communicate(timeout=10)
+        self.smbios = "type=1,product=TIMOS:address=10.0.0.15/24@active license-file=tftp://10.0.0.2/license.txt slot=A chassis=SR-c12 card=cfm-xp-b mda/1=m20-1gb-xp-sfp mda/3=m20-1gb-xp-sfp mda/5=m20-1gb-xp-sfp"
 
 
 
     def bootstrap_spin(self):
         """ This function should be called periodically to do work.
-
-            It can be used when you don't want to block waiting for the router
-            to boot, like when you are booting multiple routers in parallel.
-
-            returns True, True      when it's done and succeeded
-            returns True, False     when it's done but failed
-            returns False, False    when there is still work to be done
         """
 
         if self.spins > 60:
@@ -253,7 +94,7 @@ class SROS(vrnetlab.VR):
             self.stop_vm()
             self.start_vm()
             self.spins = 0
-            return False
+            return
 
         (ridx, match, res) = self.tn.expect([b"Login:", b"^[^ ]+#"], 1)
         if match: # got a match!
@@ -263,13 +104,13 @@ class SROS(vrnetlab.VR):
                 self.wait_write("admin", wait="Password:")
             # run main config!
             self.bootstrap_config()
-            self.vm_started = True
+            # close telnet connection
             self.tn.close()
             # calc startup time
-            startup_time = datetime.datetime.now() - start_time
+            startup_time = datetime.datetime.now() - self.start_time
             self.logger.info("Startup complete in: %s" % startup_time)
-            self.update_health(0, "SROS started")
-            return True
+            self.running = True
+            return
 
         # no match, if we saw some output from the router it's probably
         # booting, so let's give it some more time
@@ -280,7 +121,7 @@ class SROS(vrnetlab.VR):
 
         self.spins += 1
 
-        return True
+        return
 
 
     def bootstrap_config(self):
@@ -308,15 +149,35 @@ class SROS(vrnetlab.VR):
 
 
 
-    def wait_write(self, cmd, wait='#'):
-        """ Wait for something and then send command
+    def read_license(self):
+        """ Read the license file, if it exists, and extract the UUID and start
+            time of the license
         """
-        if wait:
-            self.logger.trace("Waiting for %s" % wait)
-            res = self.tn.read_until(wait.encode())
-            self.logger.trace("Read: %s" % res.decode())
-        self.logger.debug("writing to serial console: %s" % cmd)
-        self.tn.write("{}\r".format(cmd).encode())
+        if not os.path.isfile("/tftpboot/license.txt"):
+            self.logger.info("No license file found")
+            return
+
+        lic_file = open("/tftpboot/license.txt", "r")
+        license = lic_file.read()
+        lic_file.close()
+        try:
+            uuid_input = license.split(" ")[0]
+            self.uuid = mangle_uuid(uuid_input)
+            m = re.search("([0-9]{4}-[0-9]{2}-)([0-9]{2})", license)
+            if m:
+                self.fake_start_date = "%s%02d" % (m.group(1), int(m.group(2))+1)
+        except:
+            raise ValueError("Unable to parse license file")
+        self.logger.info("License file found for UUID %s with start date %s" % (self.uuid, self.fake_start_date))
+
+
+
+
+class SROS(vrnetlab.VR):
+    def __init__(self, username, password):
+        super(SROS, self).__init__(username, password)
+
+        self.vms = [ SROS_vm(username, password) ]
 
 
 
@@ -336,7 +197,5 @@ if __name__ == '__main__':
     if args.trace:
         logger.setLevel(1)
 
-    ia = SROS()
-    ia.username = args.username
-    ia.password = args.password
+    ia = SROS(args.username, args.password)
     ia.start()

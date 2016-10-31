584eca07d10a1426903d32abfcbe774f7dc82653
Kristian Larsson <kristian@spritelink.net>
2016-10-31 16:39:14 +0100

    sros: switch to m20 MDA
    Switching back to m20 MDA (but uniformly this time) since we are actually
    supposed to be able to do more than 5 ports per VM (although not working right
    now) so I reckon we stick to m20 to make it easier to troubleshoot the
    low-port-count issue.

--- sros/docker/launch.py
+++ sros/docker/launch.py
@@ -143,7 +143,7 @@ class SROS_integrated(SROS_vm):
         super(SROS_integrated, self).__init__(username, password)
 
         self.num_nics = 5
-        self.smbios = ["type=1,product=TIMOS:address=10.0.0.15/24@active license-file=tftp://10.0.0.2/license.txt slot=A chassis=SR-c12 card=cfm-xp-b mda/1=m5-1gb-sfp-b"]
+        self.smbios = ["type=1,product=TIMOS:address=10.0.0.15/24@active license-file=tftp://10.0.0.2/license.txt slot=A chassis=SR-c12 card=cfm-xp-b mda/1=m20-1gb-xp-sfp"]
 
 
 
@@ -178,7 +178,7 @@ class SROS_integrated(SROS_vm):
         self.wait_write("configure card 1 no card-type")
         self.wait_write("configure card 1 card-type iom-xp-b")
         self.wait_write("configure card 1 mcm 1 mcm-type mcm-xp")
-        self.wait_write("configure card 1 mda 1 mda-type m5-1gb-sfp-b")
+        self.wait_write("configure card 1 mda 1 mda-type m20-1gb-xp-sfp")
         self.wait_write("configure card 1 no shutdown")
         self.wait_write("admin save")
         self.wait_write("logout")

a5cf77e87c9e9c146f6215578ccc5c002226b46a
Kristian Larsson <kll@dev.terastrm.net>
2017-04-21 12:38:45 +0000

    Refactor IP config
    By reusing the same function for IPv4 and IPv6 we can essentially half the
    amount of code which is nice.

--- vr-bgp/vr-bgp.py
+++ vr-bgp/vr-bgp.py
@@ -21,6 +21,34 @@ signal.signal(signal.SIGTERM, handle_SIGTERM)
 signal.signal(signal.SIGCHLD, handle_SIGCHLD)
 
 
+def config_ip(config, input_net, man_address):
+    """ Configure IP address on the tap0 interface and set default route
+
+        This function is AFI agnostic, just feed it ipaddress objects
+    """
+    net = ipaddress.ip_network(input_net)
+    if net.prefixlen == (net.max_prefixlen-1):
+        address = net[0]
+        neighbor = net[1]
+    else:
+        address = net[1]
+        neighbor = net[2]
+
+    if man_address:
+        if ipaddress.ip_address(man_address) not in net:
+            print("Address {} not in network {}".format(man_address, net), file=sys.stderr)
+            sys.exit(1)
+        address = man_address
+
+    print("network: {}  using address: {}".format(net, address))
+
+    config['IPV{}_LOCAL_ADDRESS'.format(net.version)] = address
+    config['IPV{}_NEIGHBOR'.format(net.version)] = neighbor
+
+    subprocess.check_call(["ip", "-{}".format(net.version), "address", "add", str(address) + "/" + str(net.prefixlen), "dev", "tap0"])
+
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='')
@@ -67,36 +95,9 @@ if __name__ == '__main__':
 
     subprocess.check_call(["ip", "link", "set", "tap0", "up"])
     if args.ipv4_prefix:
-        ipv4_net = ipaddress.IPv4Network(args.ipv4_prefix)
-        if ipv4_net.prefixlen == 31:
-            ipv4_address = ipv4_net[0]
-            ipv4_neighbor = ipv4_net[1]
-        else:
-            ipv4_address = ipv4_net[1]
-            ipv4_neighbor = ipv4_net[2]
+        config_ip(config, args.ipv4_prefix,
+            args.ipv4_local_address)
 
-        if args.ipv4_local_address:
-            if ipaddress.IPv4Address(args.ipv4_local_address) not in ipv4_net:
-                print("--ipv4-local-address {} is not in --ipv4-prefix {}".format(args.ipv4_local_address, args.ipv4_prefix), file=sys.stderr)
-                sys.exit(1)
-            ipv4_address = ipaddress.IPv4Address(args.ipv4_local_address)
-
-        if args.ipv4_neighbor:
-            if ipaddress.IPv4Address(args.ipv4_neighbor) not in ipv4_net:
-                print("--ipv4-neighbor {} is not in --ipv4-prefix {}".format(args.ipv4_neighbor, args.ipv4_prefix), file=sys.stderr)
-                sys.exit(1)
-            ipv4_neighbor = ipaddress.IPv4Address(args.ipv4_neighbor)
-
-        if args.ipv4_neighbor is not None and args.ipv4_local_address == args.ipv4_neighbor:
-            print("--ipv4-neighbor {} cannot be the same as --ipv4-local-address".format(args.ipv4_neighbor, args.ipv4_local_address), file=sys.stderr)
-            sys.exit(1)
-
-        print("IPv4 network: {}  using IPv4 address: {} and IPv4 neighbor: {}".format(ipv4_net, ipv4_address, ipv4_neighbor))
-
-        config['IPV4_LOCAL_ADDRESS'] = ipv4_address
-        config['IPV4_NEIGHBOR'] = ipv4_neighbor
-
-        subprocess.check_call(["ip", "address", "add", str(ipv4_address) + "/" + str(ipv4_net.prefixlen), "dev", "tap0"])
     else:
         if args.ipv4_local_address:
             print("--ipv4-local-address requires --ipv4-prefix to be specified", file=sys.stderr)
@@ -104,36 +105,9 @@ if __name__ == '__main__':
 
 
     if args.ipv6_prefix:
-        ipv6_net = ipaddress.IPv6Network(args.ipv6_prefix)
-        if ipv6_net.prefixlen == 127:
-            ipv6_address = ipv6_net[0]
-            ipv6_neighbor = ipv6_net[1]
-        else:
-            ipv6_address = ipv6_net[1]
-            ipv6_neighbor = ipv6_net[2]
+        config_ip(config, args.ipv6_prefix,
+            args.ipv6_local_address)
 
-        if args.ipv6_local_address:
-            if ipaddress.IPv6Address(args.ipv6_local_address) not in ipv6_net:
-                print("--ipv6-local-address {} is not in --ipv6-prefix {}".format(args.ipv6_local_address, args.ipv6_prefix), file=sys.stderr)
-                sys.exit(1)
-            ipv6_address = ipaddress.IPv6Address(args.ipv6_local_address)
-
-        if args.ipv6_neighbor:
-            if ipaddress.IPv6Address(args.ipv6_neighbor) not in ipv6_net:
-                print("--ipv6-neighbor {} is not in --ipv6-prefix {}".format(args.ipv6_neighbor, args.ipv6_prefix), file=sys.stderr)
-                sys.exit(1)
-            ipv6_neighbor = ipaddress.IPv6Address(args.ipv6_neighbor)
-
-        if args.ipv6_neighbor is not None and args.ipv6_local_address == args.ipv6_neighbor:
-            print("--ipv6-neighbor {} cannot be the same as --ipv6-local-address".format(args.ipv6_neighbor, args.ipv6_local_address), file=sys.stderr)
-            sys.exit(1)
-
-        print("IPv6 network: {}  using IPv6 address: {} and IPv6 neighbor: {}".format(ipv6_net, ipv6_address, ipv6_neighbor))
-
-        config['IPV6_LOCAL_ADDRESS'] = ipv6_address
-        config['IPV6_NEIGHBOR'] = ipv6_neighbor
-
-        subprocess.check_call(["ip", "address", "add", str(ipv6_address) + "/" + str(ipv6_net.prefixlen), "dev", "tap0"])
     else:
         if args.ipv6_local_address:
             print("--ipv6-local-address requires --ipv6-prefix to be specified", file=sys.stderr)

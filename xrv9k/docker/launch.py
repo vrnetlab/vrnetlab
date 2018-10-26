593314b293ef79ceb52a54e1dd5fac6ed17aebd5
Kris Lambrechts <klambrec@dev.terastrm.net>
2018-10-26 09:06:06 +0000

    Increase XRv9k to 128 interfaces

--- xrv9k/docker/launch.py
+++ xrv9k/docker/launch.py
@@ -38,7 +38,7 @@ class XRV_vm(vrnetlab.VM):
             if re.search(".qcow2", e):
                 disk_image = "/" + e
         super(XRV_vm, self).__init__(username, password, disk_image=disk_image, ram=12288)
-        self.num_nics = 19
+        self.num_nics = 128
         self.qemu_args.extend(["-cpu", "host",
                                "-smp", "cores=4,threads=1,sockets=1",
                                "-serial", "telnet:0.0.0.0:50%02d,server,nowait" % (self.num + 1),

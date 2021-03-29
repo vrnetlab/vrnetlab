7df92eab4cc9d96e56ce7ee65782067a285b97c6
Roman Dodin <dodin.roman@gmail.com>
2021-03-29 12:08:32 +0200

    applied black

--- routeros/docker/launch.py
+++ routeros/docker/launch.py
@@ -6,15 +6,17 @@ import os
 import re
 import signal
 import sys
-import telnetlib
 
 import vrnetlab
+
 
 def handle_SIGCHLD(signal, frame):
     os.waitpid(-1, os.WNOHANG)
 
+
 def handle_SIGTERM(signal, frame):
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, handle_SIGTERM)
 signal.signal(signal.SIGTERM, handle_SIGTERM)
@@ -22,11 +24,16 @@ signal.signal(signal.SIGCHLD, handle_SIGCHLD)
 
 TRACE_LEVEL_NUM = 9
 logging.addLevelName(TRACE_LEVEL_NUM, "TRACE")
+
+
 def trace(self, message, *args, **kws):
     # Yes, logger takes its '*args' as 'args'.
     if self.isEnabledFor(TRACE_LEVEL_NUM):
         self._log(TRACE_LEVEL_NUM, message, args, **kws)
+
+
 logging.Logger.trace = trace
+
 
 class ROS_vm(vrnetlab.VM):
     def __init__(self, username, password):
@@ -39,8 +46,7 @@ class ROS_vm(vrnetlab.VM):
         self.num_nics = 31
 
     def bootstrap_spin(self):
-        """ This function should be called periodically to do work.
-        """
+        """This function should be called periodically to do work."""
 
         if self.spins > 300:
             # too many spins with no result ->  give up
@@ -49,8 +55,8 @@ class ROS_vm(vrnetlab.VM):
             return
 
         (ridx, match, res) = self.tn.expect([b"MikroTik Login"], 1)
-        if match: # got a match!
-            if ridx == 0: # login
+        if match:  # got a match!
+            if ridx == 0:  # login
                 self.logger.debug("VM started")
 
                 # Login
@@ -58,7 +64,9 @@ class ROS_vm(vrnetlab.VM):
                 # Append +ct to username for the plain-text console version
                 self.wait_write("admin+ct", wait="MikroTik Login: ")
                 self.wait_write("", wait="Password: ")
-                self.wait_write("n", wait="Do you want to see the software license? [Y/n]: ")
+                self.wait_write(
+                    "n", wait="Do you want to see the software license? [Y/n]: "
+                )
 
                 self.logger.debug("Login completed")
 
@@ -75,7 +83,7 @@ class ROS_vm(vrnetlab.VM):
 
         # no match, if we saw some output from the router it's probably
         # booting, so let's give it some more time
-        if res != b'':
+        if res != b"":
             self.logger.trace("OUTPUT: %s" % res.decode())
             # reset spins if we saw some output
             self.spins = 0
@@ -85,25 +93,36 @@ class ROS_vm(vrnetlab.VM):
         return
 
     def bootstrap_config(self):
-        """ Do the actual bootstrap config
-        """
+        """Do the actual bootstrap config"""
         self.logger.info("applying bootstrap configuration")
-        self.wait_write("/ip address add interface=ether1 address=10.0.0.15 netmask=255.255.255.0", "[admin@MikroTik] > ")
-        self.wait_write("/user add name=%s password=\"%s\" group=full" % (self.username, self.password), "[admin@MikroTik] > ")
+        self.wait_write(
+            "/ip address add interface=ether1 address=10.0.0.15 netmask=255.255.255.0",
+            "[admin@MikroTik] > ",
+        )
+        self.wait_write(
+            '/user add name=%s password="%s" group=full'
+            % (self.username, self.password),
+            "[admin@MikroTik] > ",
+        )
         self.wait_write("\r", "[admin@MikroTik] > ")
         self.logger.info("completed bootstrap configuration")
+
 
 class ROS(vrnetlab.VR):
     def __init__(self, username, password):
         super(ROS, self).__init__(username, password)
-        self.vms = [ ROS_vm(username, password) ]
+        self.vms = [ROS_vm(username, password)]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--trace', action='store_true', help='enable trace level logging')
-    parser.add_argument('--username', default='vrnetlab', help='Username')
-    parser.add_argument('--password', default='VR-netlab9', help='Password')
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "--trace", action="store_true", help="enable trace level logging"
+    )
+    parser.add_argument("--username", default="vrnetlab", help="Username")
+    parser.add_argument("--password", default="VR-netlab9", help="Password")
     args = parser.parse_args()
 
     LOG_FORMAT = "%(asctime)s: %(module)-10s %(levelname)-8s %(message)s"

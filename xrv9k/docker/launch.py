2ff79a4b317dd2d03ed90ca42021d6b084e4c401
Nathan Gotz <nathan.gotz@gmail.com>
2022-08-12 16:14:08 +0000

    xrv9k - added and tested startup-config option

--- xrv9k/docker/launch.py
+++ xrv9k/docker/launch.py
@@ -11,6 +11,8 @@ import time
 
 import vrnetlab
 
+STARTUP_CONFIG_FILE = "/config/startup-config.cfg"
+
 
 def handle_SIGCHLD(signal, frame):
     os.waitpid(-1, os.WNOHANG)
@@ -169,7 +171,7 @@ class XRV_vm(vrnetlab.VM):
                     self.stop()
                     self.start()
                     return
-
+                self.startup_config()
                 # close telnet connection
                 self.tn.close()
                 # startup time?
@@ -248,6 +250,30 @@ class XRV_vm(vrnetlab.VM):
         self.wait_write("exit")
 
         return True
+
+    def startup_config(self):
+        """Load additional config provided by user."""
+
+        if not os.path.exists(STARTUP_CONFIG_FILE):
+            self.logger.trace(f"Startup config file {STARTUP_CONFIG_FILE} is not found")
+            return
+
+        self.logger.trace(f"Startup config file {STARTUP_CONFIG_FILE} exists")
+        with open(STARTUP_CONFIG_FILE) as file:
+            config_lines = file.readlines()
+            config_lines = [line.rstrip() for line in config_lines]
+            self.logger.trace(f"Parsed startup config file {STARTUP_CONFIG_FILE}")
+
+        self.logger.info(f"Writing lines from {STARTUP_CONFIG_FILE}")
+
+        self.wait_write("configure")
+        # Apply lines from file
+        for line in config_lines:
+            self.wait_write(line)
+        # Commit and GTFO
+        self.wait_write("commit")
+        self.wait_write("exit")
+
 
     def _wait_config(self, show_cmd, expect):
         """Some configuration takes some time to "show up".

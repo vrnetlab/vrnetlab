444a23d804feb617923ebb8877b19bf4af389e86
Tom Flynn <tom.flynn@gmail.com>
2024-08-02 00:23:46 -0700

    POAP prompt has changed slightly in recent n9kv releases (#240)
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>

--- n9kv/docker/launch.py
+++ n9kv/docker/launch.py
@@ -87,9 +87,9 @@ class N9KV_vm(vrnetlab.VM):
             self.start()
             return
 
-        (ridx, match, res) = self.tn.expect([b"\(yes\/skip\/no\)\[no\]:",b"\(yes\/no\)\[n\]:", b"login:"], 1)
+        (ridx, match, res) = self.tn.expect([b"\(yes\/skip\/no\)\[no\]:",b"\(yes\/no\)\[n\]:", b"\(yes\/no\)\[no\]:", b"login:"], 1)
         if match:  # got a match!
-            if ridx == 0 or ridx == 1:
+            if ridx in (0, 1, 2):
                 self.logger.debug("matched poap prompt")
                 self.wait_write("yes", wait=None)
                 self.wait_write(
@@ -100,7 +100,7 @@ class N9KV_vm(vrnetlab.VM):
                 self.wait_write(
                     "no", wait="Would you like to enter the basic configuration dialog"
                 )
-            elif ridx == 2:  # login
+            elif ridx == 3:  # login
                 self.logger.debug("matched login prompt")
                 self.logger.debug(f'trying to log in with "admin" / {self.password}')
                 self.wait_write("admin", wait=None)
